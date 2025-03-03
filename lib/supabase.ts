eaa11f062a69b21963f716cccdc01dd7b4ce379a
elmigue21 <gueljohnc@gmail.com>
2025-03-03 20:38:42 +0800

    made supabase public

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,11 @@
 import "./config";
 import { createClient } from "@supabase/supabase-js";
 
-console.log("Supabase URL:", process.env.SUPABASE_URL);
-console.log("Supabase Anon Key:", process.env.SUPABASE_ANON_KEY);
+console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("Supabase Anon Key:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase environment variables are missing.");
