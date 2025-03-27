ba13a86c84691ab38da55d18fe077ac2dd85e598
Adagiooooo <163660350+Adagiooooo@users.noreply.github.com>
2025-03-27 19:36:11 +0800

    maangas uli na login page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,8 +2,7 @@
 'use client'
 import React from 'react'
 import Dropdown_Profie from './dropdown_profile'
-import Link from 'next/link'
-// import img from 'next/img'
+
 
 
 
@@ -11,81 +10,35 @@ import Link from 'next/link'
 const Navbar = () => {
 
   return (
-    <div className="z-50">
+    <div className='z-50'>
       <div className="fixed left-0 top-0 h-25 w-full bg-white text-black py-4 shadow-[0px_2px_10px_-3px_#000000] z-50 flex items-center px-4">
         <div className="fixed left-0 top-0 h-25 w-40 bg-[#1e2e80] z-50 flex justify-center items-center">
-          <ul>
-            <img
-              src="menu-burger.png"
-              alt="Hamburger Menu"
-              className="w-10 h-8"
-            />
-          </ul>
+        <ul><img src="menu-burger.png" alt="Hamburger Menu" className="w-10 h-8"/></ul>
         </div>
 
-        <div className="flex items-center gap-x-3  mx-auto fixed left-50">
-          <img src="dempaLogo.png" alt="Logo" className="h-20 w-20" />
-          <img src="dempaLogoTxt.png" alt="Logo Text" className="w-42 h-9" />
-
-          <div className="justify-center fixed right-20">
-            <Dropdown_Profie></Dropdown_Profie>
-          </div>
+      <div className="flex items-center gap-x-3  mx-auto fixed left-50">
+        <img src="dempaLogo.png" alt="Logo" className="h-20 w-20"/>
+        <img src="dempaLogoTxt.png" alt="Logo Text" className="w-42 h-9"/>
+        
+        <div className='justify-center fixed right-20'>
+        <Dropdown_Profie></Dropdown_Profie>
         </div>
+      
       </div>
-      <div
-        style={{ fontFamily: "Inter, sans-serif" }}
-        className="fixed left-0 top-25 h-full w-40 bg-[#1e2e80] text-white flex flex-col py-12  z-40 text-m gap-y-12"
-      >
-        <Link href="/dashboardPage">
-          <div className="flex flex-row justify-center gap-x-3 hover:bg-white hover:bg-opacity-30 rounded-2xl p-3">
-            {" "}
-            <img
-              src="layout-fluid.png"
-              alt="Dashboard Icon"
-              className="w-8 h-8"
-            />
-            <h2 className="items-center mt-1">Dashboard</h2>
-          </div>
-        </Link>
-        <div className="flex flex-row justify-center gap-x-3 ms-4 -my-3">
-          {" "}
-          <img
-            src="chart-histogram.png"
-            alt="Subscription Stats"
-            className="w-8 h-8 mt-3"
-          />
-          <h2 className="items-center mt-1">Subscription Stats</h2>
-        </div>
-        <div className="flex flex-row justify-center gap-x-3">
-          {" "}
-          <img src="refresh.png" alt="User Update" className="w-8 h-8" />
-          <h2 className="items-center mt-1">User Update</h2>
-          </div>
-        <div className="flex flex-row justify-center gap-x-3">
-          {" "}
-          <img src="user-add.png" alt="Add Account" className="w-8 h-8" />
-          <h2 className="items-center mt-1">Add Account</h2>
-        </div>
-        <div>
-          <hr className="border-t-2 border-white my-4 w-35 mx-auto opacity-50" />
-        </div>
-        <div className="flex flex-row justify-center gap-x-3 text-xl">
-          {" "}
-          <img src="circle-user.png" alt="Account" className="w-10 h-10" />
-          <h2 className="items-center mt-1">Account</h2>
-        </div>
-        <div className="flex flex-row justify-center gap-x-3 text-xl me-5">
-          {" "}
-          <img src="mail-plus-circle.png" alt="Inbox" className="w-10 h-10" />
-          <h2 className="items-center mt-1">Inbox</h2>
-        </div>
-        <div className="mt-auto flex flex-row justify-center gap-x-3 text-lg me-5 pb-16">
-          {" "}
-          <img src="exit.png" alt="Inbox" className="w-9 h-9" />
-          <h2 className="items-center mt-1">Log Out</h2>
-        </div>
+      
       </div>
+       <div style={{ fontFamily: 'Inter, sans-serif' }} className="fixed left-0 top-25 h-full w-40 bg-[#1e2e80] text-white flex flex-col py-12  z-40 text-m gap-y-12 font-bold">
+          <div className='flex flex-row justify-center gap-x-3'> <img src="layout-fluid.png" alt="Dashboard Icon" className="w-8 h-8"/><h2 className='items-center mt-1'>Dashboard</h2></div>
+          <div className='flex flex-row justify-center gap-x-3 ms-4 -my-3'> <img src="chart-histogram.png" alt="Subscription Stats" className="w-8 h-8 mt-3"/><h2 className='items-center mt-1'>Subscription Stats</h2></div>
+          <div className='flex flex-row justify-center gap-x-3'> <img src="refresh.png" alt="User Update" className="w-8 h-8"/><h2 className='items-center mt-1'>User Update</h2></div>
+          <div className='flex flex-row justify-center gap-x-3'> <img src="user-add.png" alt="Add Account" className="w-8 h-8"/><h2 className='items-center mt-1'>Add Account</h2></div>
+          <div><hr className="border-t-2 border-white my-4 w-35 mx-auto opacity-50" /></div>
+          <div className='flex flex-row justify-center gap-x-3 text-xl'> <img src="circle-user.png" alt="Account" className="w-10 h-10"/><h2 className='items-center mt-1'>Account</h2></div>
+          <div className='flex flex-row justify-center gap-x-3 text-xl me-5'> <img src="mail-plus-circle.png" alt="Inbox" className="w-10 h-10"/><h2 className='items-center mt-1'>Inbox</h2></div>
+          <div className='mt-auto flex flex-row justify-center gap-x-3 text-lg me-5 pb-16'> <img src="exit.png" alt="Inbox" className="w-9 h-9"/><h2 className='items-center mt-1'>Log Out</h2></div>
+        </div>
     </div>
+    
   );
 }
 
---
+++ app/loginPage/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+function loginPage() {
+  return (
+    <div className="relative h-screen">
+      <div className="bg-[#1A3DB2] h-9 w-full"></div>
+
+      <div className="absolute inset-0 flex items-center justify-center z-50">
+        <div className="bg-[#ECE2E2A3] w-155 h-173 flex flex-col items-center justify-start text-xl rounded-lg p-7 mt-3">
+          <div className="flex items-center gap-x-3 mt-1 me-10">
+            <img src="dempaLogo.png" alt="Logo" className="h-25 w-25 " />
+            <img src="dempaLogoTxt.png" alt="Logo Text" className="w-80 h-20" />
+          </div>
+          <div className="mt-10 text-4xl font-normal">LOGIN</div>
+          
+          <input
+            type="text"
+            className="bg-[#CFCECE] text-white border text-3xl border-gray-300 p-2 rounded-lg placeholder-white w-125 h-20 my-10 pl-8"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            className="bg-[#CFCECE] text-white border text-3xl border-gray-300 p-2 rounded-lg placeholder-white w-125 h-20 pl-8"
+            placeholder="Password"
+          />
+          
+          
+          <div className="flex flex-row gap-x-52 mt-5 tracking-wide">
+            {/*shad*/}
+            <div className="flex items-center gap-x-2">
+              <Checkbox id="terms" className="bg-[#FFFFFF]"/>
+              <label
+                htmlFor="terms"
+                className="text-[#828282] text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Remember me
+              </label>
+            </div>
+            <div className="text-[#686565] text-base font-medium">Forgot Password?</div>
+
+          </div>
+          <div className="bg-[#1A3DB2] w-50 h-13 rounded-full flex items-center justify-center text-white text-3xl mt-15">Login</div>
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 left-0 w-full h-48 p-4">
+        <div className="fixed bottom-30 left-0 w-full h-10 bg-[#E85D04] p-2"></div>
+        <div className="fixed bottom-20 left-0 w-full h-10 bg-[#DC2F02] p-2"></div>
+        <div className="fixed bottom-10 left-0 w-full h-10 bg-[#D00000] p-2"></div>
+        <div className=" fixed bottom-0 left-0 w-full h-10 bg-[#9D0208] p-2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default loginPage;
