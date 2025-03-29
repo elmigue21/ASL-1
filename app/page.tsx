a88faac608ea87893bca2ecd7262ecfd960e7b16
Adagiooooo <163660350+Adagiooooo@users.noreply.github.com>
2025-03-29 17:36:29 +0800

    in progress

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,11 @@ function page() {
     <div className="h-screen w-full bg-cover bg-center bg-[url(/img/whiteAbstractBg.jpg)] filter brightness-108">
       
       <div className="flex flex-col items-center justify-center h-full">
-      
-        <img  src="/img/dempaLogo.png" alt="Dempa Logo" className="drop-shadow-lg shadow-black/90 h-125"></img>
-        <img src="/img/dempaLogoTxt.png" alt="Logo Text" className="h-25 drop-shadow-lg shadow-black/90"></img>
+        <img  src="/img/dempaLogo.png" alt="Dempa Logo" className="drop-shadow-lg shadow-black/90 h-[60vh]"></img>
+        <img src="/img/dempaLogoTxt.png" alt="Logo Text" className="h-[11.4vh] drop-shadow-lg shadow-black/90"></img>
       </div>
 
-      <Link href={"/loginPage"}><Button className="px-8 py-2 bg-red-600 rounded-full text-white text-2xl cursor-pointer hover:bg-red-700 absolute bottom-15 right-20">LOGIN</Button></Link>
-    
+      <Link href={"/loginPage"}><Button className="w-[7vw] h-[4vh] max-h-[4vh] max-w-[7vw] bg-red-600 rounded-full text-white text-[1.24vw] cursor-pointer hover:bg-red-700 absolute bottom-[6.5vh] right-[4.2vw]">LOGIN</Button></Link>
     </div>  
   )
 
