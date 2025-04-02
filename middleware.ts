05ad62bf80cd194b70a1c1825c6feabd8f784566
Adagiooooo <163660350+Adagiooooo@users.noreply.github.com>
2025-04-02 17:16:34 +0800

    idk

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { supabase } from "./lib/supabase";
 
 export const config = {
-  matcher: ["/dashboardPage","/regisPage",],
+  matcher: ["/dashboardPage","/regisPage"]
 //   runtime:'nodejs'
 };
 
