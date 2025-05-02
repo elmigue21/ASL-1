dc0b81f9db4c891239441676ef20ec842e94522f
Agayessh <yabutaleerah@gmail.com>
2025-05-02 23:51:03 +0800

    some screen size now adjusted

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 // tailwind.config.js
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       screens: {
         "xsm": "375px", 
-        sm: "425px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1440px",
+        "sm": "425px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1440px",
       },
     },
   },
