2bdf8d45c9706d82f36c8b1ba9c70d1c6099a2d8
elmigue21 <gueljohnc@gmail.com>
2025-05-06 01:09:08 +0800

    merge

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,10 @@
 // import { Subscript } from 'lucide-react'
-'use client'
-import React from 'react'
-import Dropdown_Profie from './dropdown_profile'
-import Link from 'next/link'
-import { useDispatch } from 'react-redux'
-import { setOpenState } from '@/store/slices/emailWindowSlice'
-
-
-
-
+"use client";
+import React from "react";
+import Dropdown_Profie from "./dropdown_profile";
+import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { setOpenState } from "@/store/slices/emailWindowSlice";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -17,16 +13,8 @@ const Navbar = () => {
   };
   return (
     <div className="z-50">
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <div className="fixed left-0 top-0 h-[11vh] w-full bg-white text-black shadow-[0px_2px_10px_-3px_#000000] z-50 flex items-center px-4">
-        <div className="fixed left-0 top-0 h-[11vh] w-[8.35vw] bg-[#1e2e80] z-50 flex justify-center items-center">
-=======
-=======
->>>>>>> b39cb58eb801c187a46235366604c56e5104695a
       <div className="fixed left-0 top-0 h-[11vh] w-full bg-white text-black shadow-[0px_2px_10px_-3px_#000000] z-50 flex items-center px-4 overflow-hidden">
         <div className="absolute left-0 top-0 h-[11vh] w-[8.35vw] bg-[#1e2e80] z-50 flex justify-center items-center">
->>>>>>> b39cb58eb801c187a46235366604c56e5104695a
           <ul>
             <img
               src="menu-burger.png"
@@ -50,15 +38,7 @@ const Navbar = () => {
       </div>
       <div
         style={{ fontFamily: "Inter, sans-serif" }}
-<<<<<<< HEAD
-<<<<<<< HEAD
-        className="fixed left-0 h-full w-[8.35vw] bg-[#1e2e80] text-white flex flex-col py-[5vh]  z-40 text-[0.87vw] gap-y-[5.65vh] font-bold pt-[15vh]"
-=======
         className="fixed left-0 top-[11vh] h-full w-[8.35vw] bg-[#1e2e80] text-white flex flex-col py-[5vh]  z-40 text-[0.87vw] gap-y-[5.65vh] font-bold"
->>>>>>> b39cb58eb801c187a46235366604c56e5104695a
-=======
-        className="fixed left-0 top-[11vh] h-full w-[8.35vw] bg-[#1e2e80] text-white flex flex-col py-[5vh]  z-40 text-[0.87vw] gap-y-[5.65vh] font-bold"
->>>>>>> b39cb58eb801c187a46235366604c56e5104695a
       >
         <Link href="/dashboardPage">
           <div className="flex flex-row justify-center gap-x-[0.5vw]">
@@ -142,6 +122,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
