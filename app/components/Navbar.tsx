164157210060a769bd1ff31f10be6580b0aaff2d
Aiphie <sbmagkasi@gmail.com>
2025-05-10 21:27:37 +0800

    navbar collapse

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,125 +1,160 @@
 // import { Subscript } from 'lucide-react'
 "use client";
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Dropdown_Profie from "./dropdown_profile";
 import Link from "next/link";
 import { useDispatch } from "react-redux";
 import { setOpenState } from "@/store/slices/emailWindowSlice";
 
-const Navbar = () => {
+
+const Navbar: React.FC = () => {
   const dispatch = useDispatch();
+
+  const [isOpen, setIsOpen] = useState(false);
+
   const openClicked = () => {
     dispatch(setOpenState(true));
+  
   };
+
   return (
     <div className="z-50">
-      <div className="fixed left-0 top-0 h-[11vh] w-full bg-white text-black shadow-[0px_2px_10px_-3px_#000000] z-50 flex items-center px-4 overflow-hidden">
-        <div className="absolute left-0 top-0 h-[11vh] w-[8.35vw] bg-[#1e2e80] z-50 flex justify-center items-center">
-          <ul>
-            <img
+      <div className="fixed left-0 top-0 h-[11vh] w-full bg-white text-black shadow-[0px_2px_10px_-3px_#000000] z-60 flex items-center px-4 overflow-hidden ">
+
+        
+          <div 
+          onClick={() => setIsOpen(!isOpen)} 
+          className={`transition-all duration-100 ease-in-out overflow-hidden absolute left-0 top-0 h-[11vh] bg-[#1e2e80] z-60 flex justify-center items-center hover:bg-[#2a58ad] cursor-pointer
+          ${isOpen?"w-[15vw]": "w-[5vw]"}`}>
+          
+              <img
               src="/menu-burger.png"
               alt="Hamburger Menu"
               className="w-[2vw] h-[1.7vw]"
-            />
-          </ul>
-        </div>
+              onClick={() => setIsOpen(!isOpen)}
+              />
+          
+          </div>
+          <div className={`transition-all duration-100 ease-in-out overflow-hidden flex absolute items-center gap-[1vw] ${isOpen? "left-[16vw]" : "left-[6vw]"}`}>
+              <img src="/dempaLogo.png" alt="Logo" className="h-[8.9vh] w-[4.2vw]" />
+              <img
+                src="/dempaLogoTxt.png"
+                alt="Logo Text"
+                className="w-[8.2vw] h-[4.5vh]"
+              />
+            </div>
+            
+            <div className="flex absolute items-center w-auto right-[2vw]">
+                <Dropdown_Profie></Dropdown_Profie>
+            </div>
+          <div
+            style={{ fontFamily: "Inter, sans-serif" }}
+            className={`fixed transition-all duration-100 ease-in-out overflow-hidden left-0 top-[11vh] h-full bg-[#1e2e80] text-white flex flex-col py-[5vh] z-50 text-[2vh] font-bold
+              ${isOpen? "w-[15vw]": "w-[5vw]"}
+              `}
+          >
+              <Link href="/dashboardPage">
+                <div className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                  {" "}
+                  <div>
+                    <img
+                    src="/layout-fluid.png"
+                    alt="Dashboard Icon"
+                    className="h-[3.73vh]"
+                    />
+                  </div>
+                  <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                    <h2 className="p-0 m-0">Dashboard</h2>
+                  </div>
+                  
+                </div>
+              </Link>
+              <Link href="/tablesPage">
+                <div className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                  {" "}
+                  <img
+                    src="/chart-histogram.png"
+                    alt="Subscription Stats"
+                    className="h-[3.73vh] mt-[1.3vh]"
+                  />
+                  <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                    <h2 className="items-center mt-[0.50vh]">Subscribers</h2>
+                  </div>
+                  
+                </div>
+              </Link>
+              <Link href="/uploadPage">
+                {" "}
+                <div className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                  {" "}
+                  <img
+                    src="/refresh.png"
+                    alt="User Update"
+                    className="h-[3.73vh]"
+                  />
+                  <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                    <h2 className="items-center mt-[0.50vh]">Backups</h2>
+                  </div>
+                </div>
+              </Link>
 
-        <div className="flex items-center gap-x-[0.4vw]  mx-auto absolute left-[10.44vw]">
-          <img src="/dempaLogo.png" alt="Logo" className="h-[8.9vh] w-[4.2vw]" />
-          <img
-            src="/dempaLogoTxt.png"
-            alt="Logo Text"
-            className="w-[8.2vw] h-[4.5vh]"
-          />
-          <div className="justify-center absolute left-[78vw]">
-            <Dropdown_Profie></Dropdown_Profie>
+              <Link href="/addAccPage">
+                <div className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                  {" "}
+                  <img
+                    src="/user-add.png"
+                    alt="Add Account"
+                    className="h-[3.73vh]"
+                  />
+                  <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                    <h2 className="items-center mt-[0.50vh]">Add Subscriber</h2>
+                  </div>
+                </div>{" "}
+              </Link>
+              <div>
+                <hr className="border-t-[0.3vh] border-white my-[1.50vh] w-4/5 mx-auto opacity-50" />
+              </div>
+              <div className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                {" "}
+                <img
+                  src="/circle-user.png"
+                  alt="Account"
+                  className="h-[3.73vh]"
+                />
+                <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                  <h2 className="items-center mt-[0.50vh]">Account</h2>
+                </div>
+              </div>
+              <div
+                className={`items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}
+                onClick={() => {
+                  openClicked();
+                }}
+              >
+                {" "}
+                <img
+                  src="/mail-plus-circle.png"
+                  alt="Inbox"
+                  className="h-[3.73vh]"
+                />
+                <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                  <h2 className="items-center mt-[0.50vh]">Email</h2>
+                </div>
+              </div>
+              <Link href={"/"}>
+                <div className={`mt-[9vh] items-center h-[10vh] w-full hover:bg-[#2a58ad] ${isOpen? "grid grid-cols-2 px-[2vw] ": "flex justify-center"}`}>
+                  {" "}
+                  <img src="/exit.png" alt="Inbox" className="h-[3.73vh]" />
+                  <div className={`transition-all duration-100 ease-in-out ${isOpen? "block": "hidden"}`}>
+                    <h2 className="items-center mt-1">Log Out</h2>
+                  </div>
+                </div>
+              </Link>
           </div>
-        </div>
+          
+        
       </div>
-      <div
-        style={{ fontFamily: "Inter, sans-serif" }}
-        className="fixed left-0 top-[11vh] h-full w-[8.35vw] bg-[#1e2e80] text-white flex flex-col py-[5vh]  z-40 text-[0.87vw] gap-y-[5.65vh] font-bold"
-      >
-        <Link href="/dashboardPage">
-          <div className="flex flex-row justify-center gap-x-[0.5vw]">
-            {" "}
-            <img
-              src="/layout-fluid.png"
-              alt="Dashboard Icon"
-              className="w-[1.69vw] h-[3.73vh]"
-            />
-            <h2 className="items-center mt-[0.50vh]">Dashboard</h2>
-          </div>
-        </Link>
-        <Link href="/tablesPage">
-          <div className="flex flex-row justify-center gap-x-[0.5vw] ms-[0.50vw] -my-[0.5vh]">
-            {" "}
-            <img
-              src="/chart-histogram.png"
-              alt="Subscription Stats"
-              className="w-[1.69vw] h-[3.73vh] mt-[1.3vh]"
-            />
-            <h2 className="items-center mt-[0.50vh]">Subscription Stats</h2>
-          </div>
-        </Link>
-        <Link href="/uploadPage">
-          {" "}
-          <div className="flex flex-row justify-center gap-x-[0.5vw]">
-            {" "}
-            <img
-              src="/refresh.png"
-              alt="User Update"
-              className="w-[1.69vw] h-[3.60vh]"
-            />
-            <h2 className="items-center mt-[0.50vh]">Backups</h2>
-          </div>
-        </Link>
-
-        <Link href="/addAccPage">
-          <div className="flex flex-row justify-center gap-x-[0.5vw]">
-            {" "}
-            <img
-              src="/user-add.png"
-              alt="Add Account"
-              className="w-[1.69vw] h-[3.73vh]"
-            />
-            <h2 className="items-center mt-[0.50vh]">Add Account</h2>
-          </div>{" "}
-        </Link>
-        <div>
-          <hr className="border-t-[0.3vh] border-white my-[1.50vh] w-[6.8vw] mx-auto opacity-50" />
-        </div>
-        <div className="flex flex-row justify-center gap-x-[0.6vw] text-[1.09vw]">
-          {" "}
-          <img
-            src="/circle-user.png"
-            alt="Account"
-            className="w-[2.08vw] h-[2vw]"
-          />
-          <h2 className="items-center mt-[0.50vh]">Account</h2>
-        </div>
-        <div
-          className="flex flex-row justify-center gap-x-[0.6vw] text-[1.09vw] me-[0.50vw] cursor-pointer"
-          onClick={() => {
-            openClicked();
-          }}
-        >
-          {" "}
-          <img
-            src="/mail-plus-circle.png"
-            alt="Inbox"
-            className="w-[2.08vw] h-[2.1vw]"
-          />
-          <h2 className="items-center mt-[0.5vh]">Email</h2>
-        </div>
-        <Link href={"/"}>
-          <div className="mt-[9vh] flex flex-row justify-center gap-x-[0.6vw] text-[1.09vw]">
-            {" "}
-            <img src="/exit.png" alt="Inbox" className="w-[2vw] h-[2vw]" />
-            <h2 className="items-center mt-1">Log Out</h2>
-          </div>
-        </Link>
-      </div>
+      
     </div>
   );
 };
--- app/components/dropdown_profile.tsx
+++ app/components/dropdown_profile.tsx
@@ -13,26 +13,25 @@ export default function MyDropdown() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <div className="flex flex-row justify-center gap-x-[1.1vw] cursor-pointer">
+        <div className="flex flex-row justify-center gap-x-[1.1vw] cursor-pointer items-center">
           <Avatar className="h-[5.8vh] w-[2.8vw] object-cover">
             <AvatarImage
               src="https://github.com/shadcn.png"
               alt="@shadcn"
-              className="w-[4vw] h-[6vh]"
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div
             style={{ fontFamily: "Inter, sans-serif" }}
-            className="items-center mt-[0.6vh]"
+            
           >
             <h1 className="text-[0.75vw] font-medium">User</h1>
             <h1 className="text-[0.63vw]">Admin</h1>
           </div>
           <img
             src="/down_arrow.png"
-            alt="Inbox"
-            className="w-[2vw] h-[2.4vh] items-center mt-[1.3vh] -ml-[0.32vw]"
+            alt="profile menu"
+            className="h-[2.4vh] "
           />
         </div>
       </DropdownMenuTrigger>
